308b35fb54e236d364098191b83b977deff33c78
Luccas Burani <luccas-burani@hotmail.com>
2021-03-15 17:26:38 -0300

    Chess Layer and Printing the Board

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -3,11 +3,11 @@ package boardgame;
 public class Position {
 	
 	private int row;
-	private int colum;
+	private int column;
 	
 	public Position(int row, int colum) {
 		this.row = row;
-		this.colum = colum;
+		this.column = colum;
 	}
 
 	
@@ -18,16 +18,16 @@ public class Position {
 		this.row = row;
 	}
 	public int getColum() {
-		return colum;
+		return column;
 	}
 	public void setColum(int colum) {
-		this.colum = colum;
+		this.column = colum;
 	}
 	
 	
 	@Override
 	public String toString() {
-		return row + ", " + colum;
+		return row + ", " + column;
 	}
 	
 
