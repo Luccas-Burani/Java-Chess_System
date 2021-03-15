d3188429a80c5159a1ebc814073c30c3076543e0
Luccas Burani <luccas-burani@hotmail.com>
2021-03-15 16:16:44 -0300

    First Class: Positon

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Start System");
 		
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 		
 	}
 
